2b0622a83813bc3b8c9f156e0d02f9f4d5882475
Alex Chao <chaoalex93@gmail.com>
2023-06-13 02:57:20 +0000

    map completed with some css

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
       locationData: [],
       error: false,
       errorMsg: '',
+      mapImageUrl: '',
     }
 
   }
@@ -26,54 +27,57 @@ class App extends React.Component {
       let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API}&q=${this.state.city}&format=json`;
 
       let cityDataFromAxios = await axios.get(url);
-      let { data } = cityDataFromAxios;
+      let data = cityDataFromAxios.data;
 
       if (data.length > 0) {
-        let { lat, lon, display_name } = data[0];
 
         this.setState({
           locationData: {
-            latitude: lat,
-            longitude: lon,
-            display_name: display_name
+            latitude: data[0].lat,
+            longitude: data[0].lon,
+            display_name: data[0].display_name,
           },
+          mapImageUrl: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API}&center=${data[0].lat},${data[0].lon}&zoom=10`,
           error: false,
           errorMsg: ''
         })
       } else {
         this.setState({
           error: true,
-          errorMsg: 'No Results'
+          errorMsg: 'No Results',
+          mapImageUrl: '',
         });
       }
 
     } catch (error) {
       this.setState({
         error: true,
-        errorMsg: 'Error fetching data, ' + error.message
-
+        errorMsg: 'Error fetching data, ' + error.message,
+        mapImageUrl: '',
       });
     }
   }
 
   render() {
     return (
-      <>
-        <form onSubmit={this.handleGetCityInfo}>
+      <div className="app-container">
+        <h1>City Explorer</h1>
+        <form onSubmit={this.handleGetCityInfo}className="form-container">
           <label htmlFor=""> Enter a City Name:
             <input type="text" onChange={this.handleGetCityInput} />
           </label>
-          <button type="submit">Exploration Time!</button>
+          <button type="submit" className="submit-button">Exploration Time!</button>
         </form>
         {
-          this.state.error ? <p>{this.state.errorMsg}</p> :
-            <div>
-              <p>{this.state.locationData.display_name}</p>
-              <p>Latitude: {this.state.locationData.latitude}</p>
-              <p>Longitude: {this.state.locationData.longitude}</p>
+          this.state.error ? <p className="error-message">{this.state.errorMsg}</p> :
+            <div className='content'>
+              <p className="location-info">{this.state.locationData.display_name}</p>
+              <p className="location-info">Latitude: {this.state.locationData.latitude}</p>
+              <p className="location-info">Longitude: {this.state.locationData.longitude}</p>
+              {this.state.mapImageUrl && <img src={this.state.mapImageUrl} alt="City Map" className="city-map"/>}
             </div>
         }
-      </>
+      </div>
     )
   }
 }
